f0d579e881284dca1e1a5dcb3b04e6db7dfabe65
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2015-03-01 01:02:39 +0500

    Update SlugServiceProvider.php

--- src/Ivanlemeshev/Laravel4CyrillicSlug/SlugServiceProvider.php
+++ src/Ivanlemeshev/Laravel4CyrillicSlug/SlugServiceProvider.php
@@ -2,52 +2,49 @@
 
 use Illuminate\Support\ServiceProvider;
 
-class SlugServiceProvider extends ServiceProvider {
+class SlugServiceProvider extends ServiceProvider
+{
+    /**
+     * Indicates if loading of the provider is deferred.
+     *
+     * @var bool
+     */
+    protected $defer = false;
 
-	/**
-	 * Indicates if loading of the provider is deferred.
-	 *
-	 * @var bool
-	 */
-	protected $defer = false;
+    /**
+     * Bootstrap the application events.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        $this->package('ivanlemeshev/laravel4-cyrillic-slug');
+    }
 
-	/**
-	 * Bootstrap the application events.
-	 *
-	 * @return void
-	 */
-	public function boot()
-	{
-		$this->package('ivanlemeshev/laravel4-cyrillic-slug');
-	}
+    /**
+     * Register the service provider.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->booting(function () {
+            $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+            $loader->alias('Slug', 'Ivanlemeshev\Laravel4CyrillicSlug\Facades\Slug');
+        });
 
-	/**
-	 * Register the service provider.
-	 *
-	 * @return void
-	 */
-	public function register()
-	{
-		$this->app->booting(function()
-		{
-			$loader = \Illuminate\Foundation\AliasLoader::getInstance();
-			$loader->alias('Slug', 'Ivanlemeshev\Laravel4CyrillicSlug\Facades\Slug');
-		});
+        $this->app['laravel4-cyrillic-slug'] = $this->app->share(function ($app) {
+            return new Slug;
+        });
+    }
 
-		$this->app['laravel4-cyrillic-slug'] = $this->app->share(function($app)
-		{
-			return new Slug;
-		});
-	}
-
-	/**
-	 * Get the services provided by the provider.
-	 *
-	 * @return array
-	 */
-	public function provides()
-	{
-		return array();
-	}
-
+    /**
+     * Get the services provided by the provider.
+     *
+     * @return array
+     */
+    public function provides()
+    {
+        return [];
+    }
 }
