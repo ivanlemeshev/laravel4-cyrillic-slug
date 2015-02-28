4e07a679e2b9d0b63b92a7181b5844297091f224
Ivan Lemeshev <ivanlemeshev@users.noreply.github.com>
2015-03-01 01:03:18 +0500

    Update Slug.php

--- src/Ivanlemeshev/Laravel4CyrillicSlug/Facades/Slug.php
+++ src/Ivanlemeshev/Laravel4CyrillicSlug/Facades/Slug.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Support\Facades\Facade;
 
-class Slug extends Facade {
-
-	/**
-	* Get the registered name of the component.
-	*
-	* @return string
-	*/
-	protected static function getFacadeAccessor() { return 'laravel4-cyrillic-slug'; }
-
+class Slug extends Facade
+{
+    /**
+     * Get the registered name of the component.
+     *
+     * @return string
+     */
+    protected static function getFacadeAccessor()
+    {
+        return 'laravel4-cyrillic-slug';
+    }
 }
